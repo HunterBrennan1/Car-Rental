ac01eff9f4d335eacd6101da19a976df9166e6d7
Hunter <hunterbrennan15@gmail.com>
2023-03-25 09:16:24 -0400

    added react and react-bootstrap aswell as supporting files for application

---
+++ car-rental-app/src/components/Nav.js
@@ -0,0 +1,21 @@
+
+
+
+
+export const Nav = () => {
+  return (
+    <div className="navbar-container">
+      <div className="navbar-logo">
+        <h1>Car Rental</h1>
+      </div>
+      <div className="navbar-links">
+
+      </div>
+      <div className="navbar-login-signup-container">
+
+      </div>
+    </div>
+  )
+}
+
+export default Nav;
