1f8b33e2c146c652346afd80196bd8e87d0f9f31
Hunter <hunterbrennan15@gmail.com>
2023-03-26 12:37:22 -0400

    added correct form drop downs for rental section

--- car-rental-app/src/components/Rental.js
+++ car-rental-app/src/components/Rental.js
@@ -18,7 +18,15 @@ export const Rental = () => {
               <div className="rental-form">
                 <div>
                   <h4 className="rental-form-title"><img className="rental-icons" src={RentalIcon}></img>Select Your Car Type</h4>
-                  <input type="text" name="type" className="rental-form-input" placeholder="Select your car type"></input>
+                  <select type="text" name="type" className="rental-form-input" placeholder="Select your car type">
+                    <option>Select Your Car Type</option>
+                    <option>Audi A1 S-Line</option>
+                    <option>VW Golf 6</option>
+                    <option>Toyota Camary</option>
+                    <option>BMW 320 ModernLine</option>
+                    <option>Mercedes-Benz GLK</option>
+                    <option>VW Passat CC</option>
+                  </select>
                 </div>
                 <div>
                   <h4 className="rental-form-title"><img className="rental-icons" src={CalendarIcon}></img>Pick-Up Date</h4>
@@ -28,7 +36,14 @@ export const Rental = () => {
               <div className="rental-form">
                 <div>
                   <h4 className="rental-form-title"><img className="rental-icons" src={MarkerIcon}></img>Pick-Up Location</h4>
-                  <input type="text" name="type" className="rental-form-input" placeholder="Select your car type"></input>
+                  <select type="text" name="type" className="rental-form-input" placeholder="Select your car type">
+                    <option>Select Pick Up Location</option>
+                    <option>Toms River</option>
+                    <option>Seaside Heights</option>
+                    <option>LakeWood</option>
+                    <option>Atlantic City</option>
+                    <option>Newark</option>
+                  </select>
                 </div>
                 <div>
                   <h4 className="rental-form-title"><img className="rental-icons" src={CalendarIcon}></img>Drop-Off Date</h4>
@@ -38,7 +53,14 @@ export const Rental = () => {
               <div className="rental-form">
                 <div>
                   <h4 className="rental-form-title"><img className="rental-icons" src={MarkerIcon}></img>Drop-Off Location</h4>
-                  <input type="text" name="type" className="rental-form-input" placeholder="Select your car type"></input>
+                  <select type="text" name="type" className="rental-form-input" placeholder="Select your car type">
+                    <option>Select Drop Off Location</option>
+                    <option>Toms River</option>
+                    <option>Seaside Heights</option>
+                    <option>LakeWood</option>
+                    <option>Atlantic City</option>
+                    <option>Newark</option>
+                  </select>
                 </div>
                 <div>
                   <button className="search-rental-btn">Search</button>
