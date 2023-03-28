675bbc52eb67c7920375cdd0818327a16d2f97e5
Hunter <hunterbrennan15@gmail.com>
2023-03-28 00:40:04 -0400

    added second review

--- car-rental-app/src/components/reviews.js
+++ car-rental-app/src/components/reviews.js
@@ -2,6 +2,7 @@ import { Container, Row, Col } from "react-bootstrap";
 import React from 'react';
 import Coin from "../assets/img/iconmonstr-coin-2.svg"
 import Quote from "../assets/img/quote.svg";
+import Profile1 from "../assets/img/profile1.jpeg"
 
 
 export const Reviews = () => {
@@ -17,21 +18,42 @@ export const Reviews = () => {
                 <p className="review-text">Discover the positive impact we've made on our clients by reading through their testimonials. Our clients have experienced our service and results. They are eager to share their positive experience with you!</p>
               </div>
             </div>
-            <div className="review-box">
-              <div className="review-container">
-                <p className="review">"We rented a car from this website and had an amazin experience! The booking was easy and the rental rates were very affordable."</p>
-                <div className="review-profile-container">
-                  <div className="profile-img-name-location">
-                    <div>
-                      <img src={Coin} className="review-img"></img>
+            <div className="reviews">
+              <div className="review-box">
+                <div className="review-container">
+                  <p className="review">"We rented a car from this website and had an amazin experience! The booking was easy and the rental rates were very affordable."</p>
+                  <div className="review-profile-container">
+                    <div className="profile-img-name-location">
+                      <div>
+                        <img src={Profile1} className="review-img"></img>
+                      </div>
+                      <div className="review-owner-container">
+                        <h3 className="profile-review-name">Ivan Mata</h3>
+                        <h4 className="profile-review-location">California</h4>
+                      </div>
                     </div>
-                    <div className="review-owner-container">
-                      <h3 className="profile-review-name">Ivan Mata</h3>
-                      <h4 className="profile-review-location">California</h4>
+                    <div>
+                      <img src={Quote} className="review-img"></img>
                     </div>
                   </div>
-                  <div>
-                    <img src={Quote} className="review-img"></img>
+                </div>
+              </div>
+              <div className="review-box">
+                <div className="review-container">
+                  <p className="review">"We rented a car from this website and had an amazin experience! The booking was easy and the rental rates were very affordable."</p>
+                  <div className="review-profile-container">
+                    <div className="profile-img-name-location">
+                      <div>
+                        <img src={Profile1} className="review-img"></img>
+                      </div>
+                      <div className="review-owner-container">
+                        <h3 className="profile-review-name">Ivan Mata</h3>
+                        <h4 className="profile-review-location">California</h4>
+                      </div>
+                    </div>
+                    <div>
+                      <img src={Quote} className="review-img"></img>
+                    </div>
                   </div>
                 </div>
               </div>
