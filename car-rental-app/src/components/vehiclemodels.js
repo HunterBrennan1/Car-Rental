3e170407a3a32e88639005dd699ea4309256c005
Hunter <hunterbrennan15@gmail.com>
2023-04-01 13:36:17 -0400

    created info box for vehicle models with base styling

--- car-rental-app/src/components/vehiclemodels.js
+++ car-rental-app/src/components/vehiclemodels.js
@@ -1,7 +1,10 @@
 import { Routes, Route } from 'react-router-dom';
 import { Container, Row, Col } from "react-bootstrap";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Star from '../assets/img/staricon.svg'
 import "bootstrap/dist/css/bootstrap.min.css";
 import AudiA1Model from '../assets/img/audia1modelimg.png'
+import ModelsCarIcon from '../assets/img/modelscaricon.svg'
 
 export const VehicleModels = () => {
   return (
@@ -17,11 +20,45 @@ export const VehicleModels = () => {
                     <div className='models-div__box__descr__name-price'>
                       <div className='models-div__box__descr__name-price__name'>
                         <p>Audi A1</p>
-                        <span></span>
+                        <div className='models-div-star__icon'>
+                          <img className='star-icon' src={Star}></img>
+                          <img className='star-icon' src={Star}></img>
+                          <img className='star-icon' src={Star}></img>
+                          <img className='star-icon' src={Star}></img>
+                          <img className='star-icon' src={Star}></img>
+                        </div>
+                      </div>
+                      <div className='models-div__box__descr__name-price__price'>
+                        <h4>$45</h4>
+                        <p>per day</p>
                       </div>
                     </div>
-                    <div className='models-div__box__descr__name-price__details'></div>
-                    <div className='models-div__box__descr__name-price__btn'></div>
+                    <div className='models-div__box__descr__name-price__details'>
+                      <div>
+                        <div className="model-div__descr__box">
+                          <img className='models-car-icon' src={ModelsCarIcon}></img>
+                          <p className='models-descr-text'>Audi</p>
+                        </div>
+                        <div className="model-div__descr__box">
+                          <img className='models-car-icon' src={ModelsCarIcon}></img>
+                          <p className='models-descr-text'>Manual</p>
+                        </div>
+                      </div>
+                      <div>
+                        <div className="model-div__descr__box">
+                          <p className='models-descr-text'>4/5</p>
+                          <img className='models-car-icon' src={ModelsCarIcon}></img>
+
+                        </div>
+                        <div className="model-div__descr__box">
+                          <p className='models-descr-text'>Diesel</p>
+                          <img className='models-car-icon' src={ModelsCarIcon}></img>
+                        </div>
+                      </div>
+                    </div>
+                    <div className='models-div__box__descr__name-price__btn'>
+                      <a href="/home">Book Rides</a>
+                    </div>
                   </div>
                 </div>
 
