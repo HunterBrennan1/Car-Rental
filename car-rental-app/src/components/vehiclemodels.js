e0d0723b58b19c206b3e2137eb73bf709d0d73f4
Hunter <hunterbrennan15@gmail.com>
2023-04-01 14:50:03 -0400

    duplicated car model boxes

--- car-rental-app/src/components/vehiclemodels.js
+++ car-rental-app/src/components/vehiclemodels.js
@@ -8,65 +8,311 @@ import ModelsCarIcon from '../assets/img/modelscaricon.svg'
 
 export const VehicleModels = () => {
   return (
-    <section>
+    <section className='models-section-wrapper'>
       <Container>
-        <Row>
-          <Col>
-            <div className='models-div'>
-              <div className='models-div__box'>
-                <div className='models-div__box__img'>
-                  <img src={AudiA1Model}></img>
-                  <div className='models-div__box__descr'>
-                    <div className='models-div__box__descr__name-price'>
-                      <div className='models-div__box__descr__name-price__name'>
-                        <p>Audi A1</p>
-                        <div className='models-div-star__icon'>
-                          <img className='star-icon' src={Star}></img>
-                          <img className='star-icon' src={Star}></img>
-                          <img className='star-icon' src={Star}></img>
-                          <img className='star-icon' src={Star}></img>
-                          <img className='star-icon' src={Star}></img>
-                        </div>
-                      </div>
-                      <div className='models-div__box__descr__name-price__price'>
-                        <h4>$45</h4>
-                        <p>per day</p>
-                      </div>
+        <div className='models-div'>
+          <div className='models-div__box'>
+            <div className='models-div__box__img'>
+              <img src={AudiA1Model}></img>
+              <div className='models-div__box__descr'>
+                <div className='models-div__box__descr__name-price'>
+                  <div className='models-div__box__descr__name-price__name'>
+                    <p>Audi A1</p>
+                    <div className='models-div-star__icon'>
+                      <img className='star-icon' src={Star}></img>
+                      <img className='star-icon' src={Star}></img>
+                      <img className='star-icon' src={Star}></img>
+                      <img className='star-icon' src={Star}></img>
+                      <img className='star-icon' src={Star}></img>
                     </div>
-                    <div className='models-div__box__descr__name-price__details'>
-                      <div>
-                        <div className="model-div__descr__box">
-                          <img className='models-car-icon' src={ModelsCarIcon}></img>
-                          <p className='models-descr-text'>Audi</p>
-                        </div>
-                        <div className="model-div__descr__box">
-                          <img className='models-car-icon' src={ModelsCarIcon}></img>
-                          <p className='models-descr-text'>Manual</p>
-                        </div>
-                      </div>
-                      <div>
-                        <div className="model-div__descr__box">
-                          <p className='models-descr-text'>4/5</p>
-                          <img className='models-car-icon' src={ModelsCarIcon}></img>
+                  </div>
+                  <div className='models-div__box__descr__name-price__price'>
+                    <h4>$45</h4>
+                    <p>per day</p>
+                  </div>
+                </div>
+                <div className='models-div__box__descr__name-price__details'>
+                  <div>
+                    <div className="model-div__descr__box">
+                      <img className='models-car-icon' src={ModelsCarIcon}></img>
+                      <p className='models-descr-text'>Audi</p>
+                    </div>
+                    <div className="model-div__descr__box">
+                      <img className='models-car-icon' src={ModelsCarIcon}></img>
+                      <p className='models-descr-text'>Manual</p>
+                    </div>
+                  </div>
+                  <div>
+                    <div className="model-div__descr__box">
+                      <p className='models-descr-text'>4/5</p>
+                      <img className='models-car-icon' src={ModelsCarIcon}></img>
 
-                        </div>
-                        <div className="model-div__descr__box">
-                          <p className='models-descr-text'>Diesel</p>
-                          <img className='models-car-icon' src={ModelsCarIcon}></img>
-                        </div>
-                      </div>
                     </div>
-                    <div className='models-div__box__descr__name-price__btn'>
-                      <a href="/home">Book Rides</a>
+                    <div className="model-div__descr__box">
+                      <p className='models-descr-text'>Diesel</p>
+                      <img className='models-car-icon' src={ModelsCarIcon}></img>
                     </div>
                   </div>
                 </div>
-
+                <div className='models-div__box__descr__name-price__btn'>
+                  <a href="/home">Book Rides</a>
+                </div>
               </div>
-              <div></div>
             </div>
-          </Col>
-        </Row>
+
+          </div>
+          <div className='models-div__box'>
+            <div className='models-div__box__img'>
+              <img src={AudiA1Model}></img>
+              <div className='models-div__box__descr'>
+                <div className='models-div__box__descr__name-price'>
+                  <div className='models-div__box__descr__name-price__name'>
+                    <p>Audi A1</p>
+                    <div className='models-div-star__icon'>
+                      <img className='star-icon' src={Star}></img>
+                      <img className='star-icon' src={Star}></img>
+                      <img className='star-icon' src={Star}></img>
+                      <img className='star-icon' src={Star}></img>
+                      <img className='star-icon' src={Star}></img>
+                    </div>
+                  </div>
+                  <div className='models-div__box__descr__name-price__price'>
+                    <h4>$45</h4>
+                    <p>per day</p>
+                  </div>
+                </div>
+                <div className='models-div__box__descr__name-price__details'>
+                  <div>
+                    <div className="model-div__descr__box">
+                      <img className='models-car-icon' src={ModelsCarIcon}></img>
+                      <p className='models-descr-text'>Audi</p>
+                    </div>
+                    <div className="model-div__descr__box">
+                      <img className='models-car-icon' src={ModelsCarIcon}></img>
+                      <p className='models-descr-text'>Manual</p>
+                    </div>
+                  </div>
+                  <div>
+                    <div className="model-div__descr__box">
+                      <p className='models-descr-text'>4/5</p>
+                      <img className='models-car-icon' src={ModelsCarIcon}></img>
+
+                    </div>
+                    <div className="model-div__descr__box">
+                      <p className='models-descr-text'>Diesel</p>
+                      <img className='models-car-icon' src={ModelsCarIcon}></img>
+                    </div>
+                  </div>
+                </div>
+                <div className='models-div__box__descr__name-price__btn'>
+                  <a href="/home">Book Rides</a>
+                </div>
+              </div>
+            </div>
+
+          </div>
+          <div className='models-div__box'>
+            <div className='models-div__box__img'>
+              <img src={AudiA1Model}></img>
+              <div className='models-div__box__descr'>
+                <div className='models-div__box__descr__name-price'>
+                  <div className='models-div__box__descr__name-price__name'>
+                    <p>Audi A1</p>
+                    <div className='models-div-star__icon'>
+                      <img className='star-icon' src={Star}></img>
+                      <img className='star-icon' src={Star}></img>
+                      <img className='star-icon' src={Star}></img>
+                      <img className='star-icon' src={Star}></img>
+                      <img className='star-icon' src={Star}></img>
+                    </div>
+                  </div>
+                  <div className='models-div__box__descr__name-price__price'>
+                    <h4>$45</h4>
+                    <p>per day</p>
+                  </div>
+                </div>
+                <div className='models-div__box__descr__name-price__details'>
+                  <div>
+                    <div className="model-div__descr__box">
+                      <img className='models-car-icon' src={ModelsCarIcon}></img>
+                      <p className='models-descr-text'>Audi</p>
+                    </div>
+                    <div className="model-div__descr__box">
+                      <img className='models-car-icon' src={ModelsCarIcon}></img>
+                      <p className='models-descr-text'>Manual</p>
+                    </div>
+                  </div>
+                  <div>
+                    <div className="model-div__descr__box">
+                      <p className='models-descr-text'>4/5</p>
+                      <img className='models-car-icon' src={ModelsCarIcon}></img>
+
+                    </div>
+                    <div className="model-div__descr__box">
+                      <p className='models-descr-text'>Diesel</p>
+                      <img className='models-car-icon' src={ModelsCarIcon}></img>
+                    </div>
+                  </div>
+                </div>
+                <div className='models-div__box__descr__name-price__btn'>
+                  <a href="/home">Book Rides</a>
+                </div>
+              </div>
+            </div>
+
+          </div>
+          <div className='models-div__box'>
+            <div className='models-div__box__img'>
+              <img src={AudiA1Model}></img>
+              <div className='models-div__box__descr'>
+                <div className='models-div__box__descr__name-price'>
+                  <div className='models-div__box__descr__name-price__name'>
+                    <p>Audi A1</p>
+                    <div className='models-div-star__icon'>
+                      <img className='star-icon' src={Star}></img>
+                      <img className='star-icon' src={Star}></img>
+                      <img className='star-icon' src={Star}></img>
+                      <img className='star-icon' src={Star}></img>
+                      <img className='star-icon' src={Star}></img>
+                    </div>
+                  </div>
+                  <div className='models-div__box__descr__name-price__price'>
+                    <h4>$45</h4>
+                    <p>per day</p>
+                  </div>
+                </div>
+                <div className='models-div__box__descr__name-price__details'>
+                  <div>
+                    <div className="model-div__descr__box">
+                      <img className='models-car-icon' src={ModelsCarIcon}></img>
+                      <p className='models-descr-text'>Audi</p>
+                    </div>
+                    <div className="model-div__descr__box">
+                      <img className='models-car-icon' src={ModelsCarIcon}></img>
+                      <p className='models-descr-text'>Manual</p>
+                    </div>
+                  </div>
+                  <div>
+                    <div className="model-div__descr__box">
+                      <p className='models-descr-text'>4/5</p>
+                      <img className='models-car-icon' src={ModelsCarIcon}></img>
+
+                    </div>
+                    <div className="model-div__descr__box">
+                      <p className='models-descr-text'>Diesel</p>
+                      <img className='models-car-icon' src={ModelsCarIcon}></img>
+                    </div>
+                  </div>
+                </div>
+                <div className='models-div__box__descr__name-price__btn'>
+                  <a href="/home">Book Rides</a>
+                </div>
+              </div>
+            </div>
+
+          </div>
+          <div className='models-div__box'>
+            <div className='models-div__box__img'>
+              <img src={AudiA1Model}></img>
+              <div className='models-div__box__descr'>
+                <div className='models-div__box__descr__name-price'>
+                  <div className='models-div__box__descr__name-price__name'>
+                    <p>Audi A1</p>
+                    <div className='models-div-star__icon'>
+                      <img className='star-icon' src={Star}></img>
+                      <img className='star-icon' src={Star}></img>
+                      <img className='star-icon' src={Star}></img>
+                      <img className='star-icon' src={Star}></img>
+                      <img className='star-icon' src={Star}></img>
+                    </div>
+                  </div>
+                  <div className='models-div__box__descr__name-price__price'>
+                    <h4>$45</h4>
+                    <p>per day</p>
+                  </div>
+                </div>
+                <div className='models-div__box__descr__name-price__details'>
+                  <div>
+                    <div className="model-div__descr__box">
+                      <img className='models-car-icon' src={ModelsCarIcon}></img>
+                      <p className='models-descr-text'>Audi</p>
+                    </div>
+                    <div className="model-div__descr__box">
+                      <img className='models-car-icon' src={ModelsCarIcon}></img>
+                      <p className='models-descr-text'>Manual</p>
+                    </div>
+                  </div>
+                  <div>
+                    <div className="model-div__descr__box">
+                      <p className='models-descr-text'>4/5</p>
+                      <img className='models-car-icon' src={ModelsCarIcon}></img>
+
+                    </div>
+                    <div className="model-div__descr__box">
+                      <p className='models-descr-text'>Diesel</p>
+                      <img className='models-car-icon' src={ModelsCarIcon}></img>
+                    </div>
+                  </div>
+                </div>
+                <div className='models-div__box__descr__name-price__btn'>
+                  <a href="/home">Book Rides</a>
+                </div>
+              </div>
+            </div>
+
+          </div>
+          <div className='models-div__box'>
+            <div className='models-div__box__img'>
+              <img src={AudiA1Model}></img>
+              <div className='models-div__box__descr'>
+                <div className='models-div__box__descr__name-price'>
+                  <div className='models-div__box__descr__name-price__name'>
+                    <p>Audi A1</p>
+                    <div className='models-div-star__icon'>
+                      <img className='star-icon' src={Star}></img>
+                      <img className='star-icon' src={Star}></img>
+                      <img className='star-icon' src={Star}></img>
+                      <img className='star-icon' src={Star}></img>
+                      <img className='star-icon' src={Star}></img>
+                    </div>
+                  </div>
+                  <div className='models-div__box__descr__name-price__price'>
+                    <h4>$45</h4>
+                    <p>per day</p>
+                  </div>
+                </div>
+                <div className='models-div__box__descr__name-price__details'>
+                  <div>
+                    <div className="model-div__descr__box">
+                      <img className='models-car-icon' src={ModelsCarIcon}></img>
+                      <p className='models-descr-text'>Audi</p>
+                    </div>
+                    <div className="model-div__descr__box">
+                      <img className='models-car-icon' src={ModelsCarIcon}></img>
+                      <p className='models-descr-text'>Manual</p>
+                    </div>
+                  </div>
+                  <div>
+                    <div className="model-div__descr__box">
+                      <p className='models-descr-text'>4/5</p>
+                      <img className='models-car-icon' src={ModelsCarIcon}></img>
+
+                    </div>
+                    <div className="model-div__descr__box">
+                      <p className='models-descr-text'>Diesel</p>
+                      <img className='models-car-icon' src={ModelsCarIcon}></img>
+                    </div>
+                  </div>
+                </div>
+                <div className='models-div__box__descr__name-price__btn'>
+                  <a href="/home">Book Rides</a>
+                </div>
+              </div>
+            </div>
+
+          </div>
+          <div></div>
+        </div>
       </Container>
     </section>
   )
