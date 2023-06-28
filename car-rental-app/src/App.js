104ab92e8e2990f14a17a510a9ead5809eaf7a9d
Hunter <hunterbrennan15@gmail.com>
2023-06-28 01:49:07 -0400

    another attempted router fix

--- car-rental-app/src/App.js
+++ car-rental-app/src/App.js
@@ -1,27 +1,29 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import NavBar from './components/Nav.js';
-import MainPage from './components/Main.js';
-import Rental from './components/Rental.js';
-import ThreeBubbles from "./components/threebubbles";
-import Models from "./components/models";
-import BreakPoint from "./components/breakpoint";
-import Package from "./components/package";
-import Reviews from "./components/reviews";
-import FAQ from "./components/faq";
-import AppStore from "./components/mobile-app";
-import Footer from "./components/footer";
-import { Routes, Route } from 'react-router-dom';
+// import MainPage from './components/Main.js';
+// import Rental from './components/Rental.js';
+// import ThreeBubbles from "./components/threebubbles";
+// import Models from "./components/models";
+// import BreakPoint from "./components/breakpoint";
+// import Package from "./components/package";
+// import Reviews from "./components/reviews";
+// import FAQ from "./components/faq";
+// import AppStore from "./components/mobile-app";
+// import Footer from "./components/footer";
 import Home from "./components/home";
 import About from './components/about';
 import VehicleModels from "./components/vehiclemodels";
 import Testimonials from "./components/testimonials";
 import Contact from "./components/contact";
-import { BrowserRouter as Router, Switch } from "react-router-dom";
+
 
 function App() {
   return (
-    <Router>
+    <section>
+      <NavBar />
+
       <div className="App">
         <Routes>
           <Route path='/home' element={<Home />}></Route>
@@ -31,7 +33,8 @@ function App() {
           <Route path='/contact' element={<Contact />}></Route>
         </Routes>
       </div>
-    </Router>
+    </section>
+
   );
 }
 
